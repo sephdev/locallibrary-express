bb937d4d38bc4c09c1d93240ad0ccadff5ee66d2
Joseph Tandingan <josephdevapps@gmail.com>
2018-09-06 15:43:26 +0800

    Part 3: Using a Database

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: { type: String, required: true, min: 3, max: 100 }
+});
+
+GenreSchema.virtual('url').get(function() {
+  return '/genre/book/' + this._id;
+});
+
+// Export model
+module.exports = mongoose.model('Genre', GenreSchema);
